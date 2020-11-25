a27edda1f8cea396573114138ef2abcf58b05293
Ricardo <liweibiao@cootaa.com>
2020-11-25 15:37:31 +0800

    去掉打包的

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // let host = 'http://neworiental.local.com'; // dev
-let host = 'https://new_oriental.lmh5.com'
+let host = 'https://new-oriental.lmh5.com'
 // let host = 'https://new-oriental-uat.vdoou.cn'; // uat
 let baseURL = '/api/wx'
 
